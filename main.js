db5ce5c0195bc2bc2db2f95672ad02f5be6b0d1a
Spotky1004 <spotky1004@gmail.com>
2020-03-19 01:44:16 +0900

    1.4.10

--- main.js
+++ main.js
@@ -13,6 +13,46 @@ $(function (){
       }
     }
   }
+  function save() {
+    var date = new Date();
+    date.setDate(date.getDate() + 2000);
+
+    var willCookie = "";
+    willCookie += "saveData=";
+    a = 0;
+    var willCookieString = '{';
+    while ((a+1) <= varData.length) {
+      willCookieString += varData[a] + ':' + eval(varData[a]) + ',';
+      a++;
+    }
+    willCookieString += '}';
+    willCookie += JSON.stringify(willCookieString);
+    willCookie += ";expires=" + date.toUTCString();
+
+    document.cookie = willCookie;
+  }
+  function load() {
+    var cookies = document.cookie.split(";");
+    for(var i in cookies) {
+      if(cookies[i].search('saveData') != -1) {
+        JSON.parse(decodeURIComponent(cookies[i].replace('saveData' + "=", "")));
+      }
+    }
+    var willCookieString = '{';
+    var cookies = document.cookie.split(";");
+    for(var i in cookies) {
+      if(cookies[i].search('CookieName') != -1) {
+        savedFile = decodeURIComponent(cookies[i].replace('CookieName' + "=", ""));
+      }
+    }
+    a = 0;
+    const obj = JSON.parse(json);
+    while ((a+1) <= varData.length) {
+      varName = varData[a];
+      eval(varName);
+      a++;
+    }
+  }
   function playerStatus() {
     if (playerExp >= playerExpNeed) {
       playerExp = playerExp - playerExpNeed;
@@ -390,7 +430,6 @@ $(function (){
   playerHitPS = 1;
   bulkOpen = 1;
   extraStatus = ['', '', '', '', '', '', '', '', '', ''];
-  thinking = 42;
 
   $("#menusWarp > div").hide();
   $("#menusWarp > div:eq(0)").show();
