fd236bb901d3bde33e00ab22845de2e771d3b4d5
Spotky1004 <spotky1004@gmail.com>
2020-04-01 21:17:49 +0900

    1.8.3

--- main.js
+++ main.js
@@ -606,8 +606,8 @@ $(function (){
         gotWeaponCalc(((lootPage-1)*5+2+a), gotWeapon[1]);
         gotWeaponCalc(((lootPage)*5), gotWeapon[2]);
       } else {
-        gotWeaponCalc(((lootPage-1)*5+a), gotWeapon[0]);
-        gotWeaponCalc(((lootPage-1)*5+1+a), gotWeapon[1]);
+        gotWeaponCalc(((lootPage-1)*5+1+a), gotWeapon[0]);
+        gotWeaponCalc(((lootPage-1)*5+a), gotWeapon[1]);
         gotWeaponCalc(((lootPage)*5), gotWeapon[2]);
       }
     }
