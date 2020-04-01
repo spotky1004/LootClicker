2e67616e515a8bb80ec684a104c94203c4feb77d
Spotky1004 <spotky1004@gmail.com>
2020-04-02 02:46:42 +0900

    1.8.5

--- main.js
+++ main.js
@@ -518,10 +518,10 @@ $(function (){
     if (a != 3 && a != 4) {
       menuCheck = 1;
     } else if (a == 3) {
-      if (playerLevel >= 31) {
+      if (playerLevel >= 21) {
         menuCheck = 1;
       } else {
-        setPopup(popupMsg[1] + ' ' + 31 + ' ' + popupMsg[2]);
+        setPopup(popupMsg[1] + ' ' + 21 + ' ' + popupMsg[2]);
       }
     } else if (a == 4) {
       if (playerLevel >= 101) {
