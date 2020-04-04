1af643a5b2be8fd52cb67a252de6cacf9d1ab4af
Spotky1004 <spotky1004@gmail.com>
2020-04-05 08:09:37 +0900

    1.9.1.30

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ $(function (){
     for(var i in cookies) {
       if(cookies[i].search('saveData') != -1) {
         const savedFile = JSON.parse(decodeURIComponent(cookies[i].replace('saveData' + "=", "")));
-        dataCopy = resetData;
+        dataCopy = JSON.parse(JSON.stringify(resetData));
         Object.assign(dataCopy, savedFile);
         for (var i = 0; i < varData.length; i++) {
           this[varData[i]] = dataCopy[i];
